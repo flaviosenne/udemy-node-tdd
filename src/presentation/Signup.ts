37dc571e500de65d350857545f9a3b05241210cb
flaviosenne <flaviosenne123@gmail.com>
2021-01-28 14:30:37 -0300

    feat: add test password

--- src/presentation/Signup.ts
+++ src/presentation/Signup.ts
@@ -1,18 +1,15 @@
 import { MissingParamError } from './errors/missing-param-error';
 import { HttpRequest,HttpResponse}from'../protocols/https'
+import { badRequest} from './helpers/http-helpers'
 
 export class SignUpController {
     handle(httpRequest: HttpRequest): HttpResponse{
-        if(!httpRequest.body.name){
-            return {
-                statusCode: 400,
-                body: new MissingParamError('name')
-            }
-        }
-        if(!httpRequest.body.email){
-            return {
-                statusCode: 400,
-                body: new MissingParamError('email')
+
+        const requiredFields = ['name', 'email', 'password']
+        for(const field of requiredFields){
+            if(!httpRequest.body[field]){
+
+                return badRequest(new MissingParamError(field))
             }
         }
     }
