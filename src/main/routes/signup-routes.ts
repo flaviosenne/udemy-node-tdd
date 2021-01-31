e7daa193594809c9192c77a01e0537ab7bf8f424
flaviosenne <flaviosenne123@gmail.com>
2021-01-31 09:28:11 -0300

    test: add user in mongo

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
+import { makeSignUpController} from '../factories/signup'
+import{ adptRoute} from '../adapters/express-routes-adapter'
 export default (router: Router): void => {
-    router.post('/signup', (req, res) => {
-        res.json({ok: 'ok'})
-    })
+    router.post('/signup', adptRoute(makeSignUpController()))
 }
