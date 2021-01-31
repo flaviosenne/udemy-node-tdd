2e00371ceb094e728b829ea2c92c58b14dc4c069
flaviosenne <flaviosenne123@gmail.com>
2021-01-30 21:14:14 -0300

    feat: ensure signup routes return an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,7 @@
 import expres from 'express'
 import setupMiddlewares from './middlewares'
-
+import setupRoutes from './routes'
 const app = expres()
 setupMiddlewares(app)
+setupRoutes(app)
 export default app
