c4abc4dfe4da32349fbc53f5fcd898f21c65117d
flaviosenne <flaviosenne123@gmail.com>
2021-02-01 07:17:30 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signup/Signup.ts
+++ src/presentation/controllers/signup/Signup.ts
@@ -42,7 +42,7 @@ export class SignUpController implements Controller {
             return ok(account)
             
         } catch (err) {
-           return serverError()
+           return serverError(err)
         }
     }
 }
--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,6 +1,7 @@
 export class ServerError extends Error {
-    constructor(){
+    constructor(stack: string){
         super('internal Server Error')
         this.name = 'Server Error'
+        this.stack = stack
     }
 }
