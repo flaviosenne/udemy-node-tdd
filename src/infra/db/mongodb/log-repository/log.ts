a0a53b061778f47f2b482fd11845aa42385f141f
flaviosenne <flaviosenne123@gmail.com>
2021-02-01 14:36:38 -0300

    feat: ensure Login return 400 if email no provided

--- src/infra/db/mongodb/log-repository/log.ts
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -3,9 +3,11 @@ import MongoHelper from '../helpers/mongo-helper'
 export class LogMongoRepository implements LogErrorRepository{
     async logError(stack: string): Promise<void>{
         const log = await MongoHelper.getCollection('log')
-        return log.create({
+        const result = log.create({
             stack,
             date: new Date()
         })
+        console.log(result)
+        return result
     }
 }
