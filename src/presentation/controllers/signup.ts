50806f9399d2c11c9f19047076668ba78c1a46c6
joao flavio <flaviosenne123@gmail.com>
2021-08-08 10:04:03 -0300

    refactor: change mocking strategy

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -11,8 +11,9 @@ export class SignUpController implements Controller {
         try {
             const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
             for (const field of requiredFields) {
-                if (!httpRequest.body[field])
+                if (!httpRequest.body[field]){
                     return badRequest(new MissingParamError(field))
+                }
             }
 
             const isValid = this.emailValidator.isValid(httpRequest.body.email)
